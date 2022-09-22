61b7929f8882905bc77ac6532594c6a7563fa4ea
GolfDev <golflovezaz0@gmail.com>
2022-09-22 09:42:20 +0700

    init commit Projact

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- <h1 className="text-3xl font-bold text-red-700">Vite App</h1> -->
+  <!-- <nav>
+    <router-link to="/">HOME|</router-link>
+    <router-link to="/about">ABOUT</router-link>
+  </nav> -->
+  <RouterView />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ src/layouts/Frontend.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+    <div>
+        <Nav />
+            <main>
+                <router-view />
+            </main>
+        <Foot />
+    </div>
+</template>
+
+<script>
+import Nav from "@/components/frontend/Nav.vue";
+import Foot from "@/components/frontend/Foot.vue";
+export default {
+  name: "FrontendLayout",
+  components: {
+    Nav,
+    Foot,
+  },
+};
+</script>
+
+<style lang=""></style>
